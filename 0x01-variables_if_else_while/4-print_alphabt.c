688a6aaab3c6abf637c74c313f3a57ac5f5741ac
EddyKG <eddy.githinji022@gmail.com>
2023-07-16 13:04:12 -0700

    prints alphs ommiting q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main- prints alphabets but q and e
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+			continue; /*skips the specified letters*/
+	putchar('\n');
+	}
+	return (0);
+}
